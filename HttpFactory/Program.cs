0bf009fcbf2eabc622c24278d33856e1ceab724b
Guilherme Flores <guilheremfloresdev@gmail.com>
2025-04-17 13:20:47 +0100

    comments

--- HttpFactory/Program.cs
+++ HttpFactory/Program.cs
@@ -3,6 +3,13 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
+// equivalent in a non-console app
+//builder.Services.AddHttpClient("SwapiClient", client =>
+//{
+//    client.BaseAddress = new Uri("https://swapi.tech/api/");
+//});
+
 using var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((_, services) =>
     {
